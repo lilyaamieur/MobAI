8765a10fe019892f851bd1b91e6e1c079998e74f
lilya <amieurlilya@gmail.com>
2025-02-13 18:15:42 +0100

    update

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_application_1/upload_page.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: 'https://mshrjmoigzxwimnnnatu.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zaHJqbW9pZ3p4d2ltbm5uYXR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMTk4MjAsImV4cCI6MjA1NDY5NTgyMH0.9oU70sil_DZAyHzbcWQEln-vyqdi5eez7Tf7JwmEr6E',
+  );
+
   runApp(const MainApp());
 }
 
@@ -12,7 +20,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: UploadPage(),
         ),
       ),
     );
