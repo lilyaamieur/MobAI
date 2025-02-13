eb8f880bc1684f4d7b9abc310f470cf0b99929cc
= <kennylalek@gmail.com>
2025-02-13 20:45:38 +0100

    sign in

--- flutter_application_1/lib/views/screens/authentication/sign_in.dart
+++ flutter_application_1/lib/views/screens/authentication/sign_in.dart
@@ -49,8 +49,6 @@ class SignUp extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(24.0),
         children: [
-          
-
           const Divider(),
           spacer,
 
@@ -62,56 +60,67 @@ class SignUp extends StatelessWidget {
                 padding: const EdgeInsets.all(30),
                 child: Theme(
                   data: darkModeThemeData,
-                  child: SupaEmailAuth(
-                      redirectTo: kIsWeb ? null : 'io.supabase.flutter://',
-                      onSignInComplete: navigateHome,
-                      onSignUpComplete: navigateHome,
-                      prefixIconEmail: null,
-                      prefixIconPassword: null,
-                      localization: const SupaEmailAuthLocalization(
-                          enterEmail: "email",
-                          enterPassword: "password",
-                          dontHaveAccount: "sign up",
-                          forgotPassword: "forgot password"),
-                      metadataFields: [
-                        MetaDataField(
-                          prefixIcon: const Icon(Icons.person),
-                          label: 'Username',
-                          key: 'username',
-                          validator: (val) {
-                            if (val == null || val.isEmpty) {
-                              return 'Please enter something';
-                            }
-                            return null;
-                          },
-                        ),
-                        BooleanMetaDataField(
-                          label:
-                              'Keep me up to date with the latest news and updates.',
-                          key: 'marketing_consent',
-                          checkboxPosition: ListTileControlAffinity.leading,
-                        ),
-                        BooleanMetaDataField(
-                          key: 'terms_agreement',
-                          isRequired: true,
-                          checkboxPosition: ListTileControlAffinity.leading,
-                          richLabelSpans: [
-                            const TextSpan(
-                                text: 'I have read and agree to the '),
-                            TextSpan(
-                              text: 'Terms and Conditions.',
-                              style: const TextStyle(
-                                color: Colors.blue,
-                              ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  //ignore: avoid_print
-                                  print('Terms and Conditions');
-                                },
+                  child: Column(
+                    children: [
+                      SupaEmailAuth(
+                          redirectTo: kIsWeb ? null : 'io.supabase.flutter://',
+                          onSignInComplete: navigateHome,
+                          onSignUpComplete: navigateHome,
+                          prefixIconEmail: null,
+                          prefixIconPassword: null,
+                          localization: const SupaEmailAuthLocalization(
+                              enterEmail: "email",
+                              enterPassword: "password",
+                              dontHaveAccount: "sign up",
+                              forgotPassword: "forgot password"),
+                          metadataFields: [
+                            MetaDataField(
+                              prefixIcon: const Icon(Icons.person),
+                              label: 'Username',
+                              key: 'username',
+                              validator: (val) {
+                                if (val == null || val.isEmpty) {
+                                  return 'Please enter something';
+                                }
+                                return null;
+                              },
                             ),
-                          ],
-                        ),
-                      ]),
+                            BooleanMetaDataField(
+                              label:
+                                  'Keep me up to date with the latest news and updates.',
+                              key: 'marketing_consent',
+                              checkboxPosition: ListTileControlAffinity.leading,
+                            ),
+                            BooleanMetaDataField(
+                              key: 'terms_agreement',
+                              isRequired: true,
+                              checkboxPosition: ListTileControlAffinity.leading,
+                              richLabelSpans: [
+                                const TextSpan(
+                                    text: 'I have read and agree to the '),
+                                TextSpan(
+                                  text: 'Terms and Conditions.',
+                                  style: const TextStyle(
+                                    color: Colors.blue,
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      //ignore: avoid_print
+                                      print('Terms and Conditions');
+                                    },
+                                ),
+                              ],
+                            ),
+                          ]),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Handle sign-up action
+                        },
+                        child: const Text('Sign Up'),
+                      ),
+                    ],
+                  ),
                 ),
               )),
 
@@ -134,7 +143,6 @@ class SignUp extends StatelessWidget {
             label: const Text('Sign in with Phone'),
           ),
           spacer,
-
         ],
       ),
     );
