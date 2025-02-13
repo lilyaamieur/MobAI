eb74793a4e887be74854b6b2a3e6adc3bd054e57
= <kennylalek@gmail.com>
2025-02-13 18:57:01 +0100

    supabase configuration

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_application_1/upload_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: '.env');
   await Supabase.initialize(
-    url: 'https://mshrjmoigzxwimnnnatu.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zaHJqbW9pZ3p4d2ltbm5uYXR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMTk4MjAsImV4cCI6MjA1NDY5NTgyMH0.9oU70sil_DZAyHzbcWQEln-vyqdi5eez7Tf7JwmEr6E',
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
   runApp(const MainApp());
